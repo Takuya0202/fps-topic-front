5d97c6ffe75cdfc8b980c55f4d417c329d881374
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-01-23 15:55:40 +0900

    設定要素の追加

--- src/app/components/buttons/game-icon.tsx
+++ src/app/components/buttons/game-icon.tsx
@@ -1,7 +1,7 @@
 export default function GameIcon() {
-    return (
-        <div>
-            <h1>Game Icon</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Game Icon</h1>
+    </div>
+  );
 }
