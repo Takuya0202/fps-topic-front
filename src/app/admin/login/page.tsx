d085b610dd3290493d509d86fa59bc8a1ee69c3d
Yuge <yuge.a.24kdgh@gmail.com>
2026-01-25 03:05:30 +0900

    ラベル変更

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -5,10 +5,8 @@ export default function Login() {
     <main className="w-full h-screen flex items-center justify-center">
       <form className="flex flex-col items-center justify-center space-y-3  ">
         <h1 className="text-2xl font-bold pb-4 mb-12 w-full text-center  ">管理者用ログイン</h1>
-        <label htmlFor="" className="text-left w-full text-[32px]">管理者ID</label>
-        <InputField label="" type="text" id="login-id" placeholder="ログインID" />
-        <label htmlFor="" className="text-left w-full text-[32px]">パスワード</label>
-        <InputField label="" type="password" id="password" placeholder="パスワード" />
+        <InputField label="管理者ID" type="text" id="login-id" placeholder="ログインID" />
+        <InputField label="パスワード" type="password" id="password" placeholder="パスワード" />
         <div className=" mt-[80px]">
           <button type="submit" className="w-40  bg-[#949494] h-[39px] border-2 border-black ">
             <p className="text-black">ログイン</p>
