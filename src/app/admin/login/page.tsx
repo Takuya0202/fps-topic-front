9b6245ef8e6c709c0d0a6eac74e6375096fb7459
Yuge <yuge.a.24kdgh@gmail.com>
2026-01-25 02:57:19 +0900

    formタグに変更

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -3,18 +3,18 @@ import InputField from "../../components/elements/input/input-field";
 export default function Login() {
   return (
     <main className="w-full h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center space-y-3  ">
+      <form className="flex flex-col items-center justify-center space-y-3  ">
         <h1 className="text-2xl font-bold pb-4 mb-12 w-full text-center  ">管理者用ログイン</h1>
         <label htmlFor="" className="text-left w-full text-[32px]">管理者ID</label>
         <InputField label="" type="text" id="login-id" placeholder="ログインID" />
         <label htmlFor="" className="text-left w-full text-[32px]">パスワード</label>
         <InputField label="" type="password" id="password" placeholder="パスワード" />
         <div className=" mt-[80px]">
-          <button className="w-40  bg-[#949494] h-[39px] border-2 border-black ">
+          <button type="submit" className="w-40  bg-[#949494] h-[39px] border-2 border-black ">
             <p className="text-black">ログイン</p>
           </button>
         </div>
-      </div>
+      </form>
     </main>
   );
 }
