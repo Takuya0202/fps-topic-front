a2fb272f9e6be585f971b818751207175a4c80c1
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2026-01-26 15:17:50 +0900

    コメントモーダルの作成

--- src/app/components/features/comments/comment-button.tsx
+++ src/app/components/features/comments/comment-button.tsx
@@ -11,16 +11,16 @@ export default function CommentButton({ articleId }: Props) {
   const [isOpen, setIsOpen] = useState(false);
   const handleClick = () => {
     setIsOpen(true);
-  }
+  };
 
   if (isOpen) {
-    return <CommentModal
-      onClose={() => setIsOpen(false)}
-      articleId={articleId}
-    />;
+    return <CommentModal onClose={() => setIsOpen(false)} articleId={articleId} />;
   }
   return (
-    <button onClick={handleClick} className="w-[56px] h-[56px] bg-crimson rounded-full flex items-center justify-center shadow">
+    <button
+      onClick={handleClick}
+      className="w-[56px] h-[56px] bg-crimson rounded-full flex items-center justify-center shadow"
+    >
       <MessageCirclePlus className="w-8 h-8 text-white" />
     </button>
   );
--- src/app/components/features/comments/comment-form.tsx
+++ src/app/components/features/comments/comment-form.tsx
@@ -1,25 +1,45 @@
+"use client";
 import { X } from "lucide-react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 interface Props {
-    onClose: () => void;
-    articleId: string;
+  onClose: () => void;
+  articleId: string;
 }
 
 export default function CommentForm({ onClose, articleId }: Props) {
-    return (
-        <form action=""
-            className="flex items-start flex-col space-y-2 p-2 bg-darker h-full">
+  const [comment, setComment] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (comment.trim() === "") {
+      toast.error("コメントが未入力です");
+      return;
+    }
+    // コメント送信APIの呼び出し
+    toast.success("コメントを送信しました");
+    setComment("");
+    onClose();
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-start flex-col space-y-2 p-4 bg-darker h-full rounded-t-[10px]"
+    >
+      <div className="flex items-center justify-between w-full">
+        <X className="w-8 h-8 text-white opacity-80" onClick={onClose} />
+        <h2 className="text-white font-bold">Comment</h2>
+        <button type="submit" className="text-crimson font-bold">
+          送信
+        </button>
+      </div>
 
-            <div className="flex items-center justify-between w-full">
-                <X className="w-5 h-5 text-white opacity-80"
-                    onClick={onClose} />
-                <h2 className="text-white font-bold">
-                    Comment
-                </h2>
-                <button type="submit" className="text-crimson font-bold">
-                    送信
-                </button>
-            </div>
-        </form>
-    )
+      <textarea
+        className="w-full h-full p-4 text-white opacity-70 font-bold placeholder:text-white/70"
+        placeholder="ここにコメントを入力"
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+      ></textarea>
+    </form>
+  );
 }
